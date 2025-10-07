674f0d8c5b2b952495f44f4649f564474d0f2ca1
Mattias Jonsson <mattias.jonsson@pingcap.com>
2025-10-08 01:41:57 +0200

    More batching for updating to avoid copr_cache

--- main_test.go
+++ main_test.go
@@ -18,12 +18,14 @@ func TestSimple(t *testing.T) {
 }
 
 func TestMulti(t *testing.T) {
-	rowCounts := []int{1000000, 10000000}
+	rowCounts := []int{1000000}
+	//rowCounts := []int{1000000, 10000000}
 	selectivities := []float64{0.02, 0.05, 0.075, 0.1, 0.15, 0.2}
 
 	// Run the optimizer tests
 	setupLogging("debug")
-	err := RunOptimizerTests(rowCounts, selectivities, 7)
+	err := RunOptimizerTests(rowCounts, selectivities, 3)
+	//err := RunOptimizerTests(rowCounts, selectivities, 7)
 	if err != nil {
 		t.Fatalf("RunOptimizerTests failed: %v", err)
 	}
